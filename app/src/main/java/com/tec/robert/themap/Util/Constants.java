f236ae63b3b43aa495998123565f631429e6dbd7
RobertGallegos100 <roberto.gallegos242@gmail.com>
2018-06-08 00:37:57 -0600

    Initial commit

---
+++ app/src/main/java/com/tec/robert/themap/Util/Constants.java
@@ -0,0 +1,12 @@
+package com.tec.robert.themap.Util;
+
+import java.util.Random;
+
+public class Constants {
+    public static final String URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson";
+    public static final int LIMIT = 25;
+
+    public static int randomInt(int max, int min){
+        return new Random().nextInt(max - min) + min;
+    }
+}
